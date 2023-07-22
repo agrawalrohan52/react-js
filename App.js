6bb40e1bb2925908d7883984b2354c723d72dc13
Rohan Agrawal <agrawalrohan52@gmail.com>
2023-07-22 17:35:41 +0530

    Episode 03-Laying the foundation

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
-console.log(parent);
+const HeadingComponent = () => (
+  <div id="container">
+    // Calling functional component inside functional component using different
+    ways
+    <Title />
+    <Title></Title>
+    {Title()}
+    // Calling react element inside functional component
+    {reactElement}
+  </div>
+);
+
+const Title = () => (
+  <h1 className="head" tabIndex="1">
+    Functional Component Title
+  </h1>
+);
+
+const reactElement = (
+  <h1 className="head" tabIndex="1">
+    // Calling functional component inside react element
+    <Title />
+    React Element
+  </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// Rendering functional component
+root.render(<HeadingComponent />);
+// Rendering react element
+// root.render(reactElement);
